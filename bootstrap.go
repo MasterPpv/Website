9393043138c742ae05484c87a9e689eaa8ca044a
MasterPpv <pritenvora@gmail.com>
2013-12-31 01:19:41 -0600

    Craeted Bootstrap file handler and prepared any necessary error messages

--- bootstrap.go
+++ bootstrap.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func init() {
@@ -10,6 +12,31 @@ func init() {
 }
 
 func bootstrapHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path;
-	fmt.Fprintf(w,"%s",path)
+	path := "." + r.URL.Path;
+	bsFile, err := os.Open(path)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(404)
+		return
+	}
+	bsFileInfo, err := bsFile.Stat()
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(500)
+		return
+	}
+	bsFileSize := bsFileInfo.Size()
+	bsFileData := make([]byte, bsFileSize, bsFileSize)
+	numBytesRead, err := bsFile.Read(bsFileData)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(500)
+		return
+	}
+	if int64(numBytesRead) != bsFileSize {
+		log.Printf("Expected %v bytes, read %v bytes\n", bsFileSize, numBytesRead)
+		w.WriteHeader(500)
+		return
+	}
+	fmt.Fprintf(w,"%s", string(bsFileData))
 }
--- homePage.go
+++ homePage.go
@@ -13,10 +13,14 @@ func init() {
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
+		w.WriteHeader(500)
+		return
 	}
 	err = tmpl.Execute(w, nil)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
+		w.WriteHeader(500)
+		return
 	}
 }
